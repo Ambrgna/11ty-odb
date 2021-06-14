c4c8c0ce1fabddd1e06794380f6a1edbcd9b78b4
kenny-io <ekeneeze3@gmail.com>
2021-06-14 19:50:03 +0000

    updated api endpoint

--- netlify/functions/shop.js
+++ netlify/functions/shop.js
@@ -2,7 +2,7 @@ const pageTemplate = require("../../_includes/templates/data");
 const fetch = require("node-fetch");
 
 exports.handler = async (event) => {
-  const result = await fetch(process.env.PRODUCTS_DATA, {
+  const result = await fetch("http://test.gasport.com.ng/API/OrderList", {
     method: "GET",
   }).then((res) => res.json());
 
