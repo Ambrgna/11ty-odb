feacfb8d4bb248d59cf88cd794cfa1ed9a0eb9f3
kenny-io <ekeneeze3@gmail.com>
2021-06-15 18:56:28 +0000

    update fns

--- netlify/functions/odb-product.js
+++ netlify/functions/odb-product.js
@@ -4,14 +4,15 @@ const fetch = require("node-fetch");
 const { builder } = require("@netlify/functions");
 
 const handler = async (event) => {
-  // console.log(event);
-  const id = event.path.split("id/")[1];
+  const id = event.path.split("product/")[1];
+
+  console.log(id);
 
   const result = await fetch(`${process.env.PRODUCTS_DATA}/${id}`, {
     method: "GET",
   })
     .then((res) => res.json())
-    .catch((error) => console.error(error));
+    .catch((error) => console.log(error));
 
   return {
     statusCode: 200,
